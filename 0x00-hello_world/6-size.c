4ebdb86f679daea4873e9d08d149c0038c7df53d
Duhawaly <duhawaly@gmail.com>
2023-09-08 13:52:53 -0700

    faf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,33 @@
-#!/bin/bash
-include <studio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
 
-    return 0;
+
+
+6-size.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+        printf("Size of a char: %lu byte(s)\n", sizeof(char));
+        printf("Size of an int: %lu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));
+        return (0);
 }
